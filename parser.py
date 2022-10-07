5b52858fca3374f9cab536fdb87c8aeab25fc1b2
Ashwin U Iyer <ashwiniyer1706@gmail.com>
2022-10-07 20:32:21 +0530

    add comments

--- parser.py
+++ parser.py
@@ -12,6 +12,7 @@ reader = Reader(["en"], gpu=True)
 
 
 def get_regno_and_serial(image):
+    # Function to get Serial Number and regno
     result = reader.readtext(image)
 
 
@@ -19,11 +20,11 @@ for i in range(0, len(img), 2):
     imlist = []
     imlist.append(Image.fromarray(img[i]))
     print(reader.readtext(img[i])[0])
-    quit()
     imlist.append(Image.fromarray(img[i + 1]))
 
+    # Saves two pages of the certificate document
     imlist[0].save(
-        f"test_{i+1}_record.tif",
+        f"test_{i+1}_record.tif",  # Change the title here
         compression="tiff_deflate",
         save_all=True,
         append_images=imlist[1:],
